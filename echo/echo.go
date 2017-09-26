6bb2e956cd6c3e3084519a7ead0026740e84f16e
Darren Noble <nobled@gmail.com>
2017-09-26 17:19:48 -0600

    initial commit with ping and echo servers and a relay server copied from ping and slightly modified

---
+++ echo/echo.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+var (
+	port = flag.Int("port", 9876, "the port to listen on")
+)
+
+const buffSize int = 1024
+
+func handleError(err error, msg string, args ...interface{}) {
+	if msg == "" {
+		msg = "Error"
+	}
+
+	if len(args) > 0 {
+		msg = fmt.Sprintf(msg, args...)
+	}
+
+	fmt.Printf("%s: %v", msg, err)
+	os.Exit(1)
+}
+
+func main() {
+	flag.Parse()
+
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%v", *port))
+	if err != nil {
+		handleError(err, "Error listening on port %v", *port)
+	}
+
+	con, err := ln.Accept()
+	if err != nil {
+		handleError(err, "Error accepting connection")
+	}
+
+	b := make([]byte, buffSize, buffSize)
+
+	var count int
+
+	for {
+		count, err = con.Read(b)
+		if err == io.EOF {
+			os.Exit(0)
+		}
+		if err != nil {
+			handleError(err, "Error reading from socket")
+		}
+		b2 := b[0:count]
+		fmt.Print(string(b2))
+
+		_, err = con.Write(b2)
+		if err != nil {
+			handleError(err, "Error writing to socket")
+		}
+	}
+}
---
+++ ping/ping.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+var (
+	host        = flag.String("host", "localhost", "the host to send to")
+	port        = flag.Int("port", 9876, "the port to send to")
+	packetCount = flag.Int("count", 20, "the number of packets to send")
+)
+
+func handleError(err error, msg string, args ...interface{}) {
+	if msg == "" {
+		msg = "Error"
+	}
+
+	if len(args) > 0 {
+		msg = fmt.Sprintf(msg, args...)
+	}
+
+	fmt.Printf("%s: %v", msg, err)
+	os.Exit(1)
+}
+
+func main() {
+	flag.Parse()
+
+	hostStr := fmt.Sprintf("%s:%v", *host, *port)
+
+	con, err := net.Dial("tcp", hostStr)
+	if err != nil {
+		handleError(err, "Error connecting to %s", hostStr)
+	}
+
+	b := make([]byte, 16, 16)
+
+	var count int
+
+	var totalTime time.Duration
+
+	for i := 1; i <= *packetCount; i++ {
+		packetStr := fmt.Sprintf("%v", i)
+
+		start := time.Now()
+
+		_, err = con.Write([]byte(packetStr))
+		if err != nil {
+			handleError(err, "Error writing to socket")
+		}
+		count, err = con.Read(b)
+
+		end := time.Now()
+
+		if err != nil {
+			handleError(err, "Error reading from socket")
+		}
+		bStr := string(b[0:count])
+		if packetStr != bStr {
+			handleError(fmt.Errorf("expected %s, got %s", packetStr, bStr), "Error, invalid response")
+		}
+
+		dur := end.Sub(start)
+		totalTime += dur
+
+		fmt.Printf("%v - %s\n", i, dur.String())
+	}
+
+	avg := totalTime / time.Duration(*packetCount)
+
+	fmt.Printf("\n%v packets sent.  Avg latency %s\n", *packetCount, avg.String())
+
+	con.Close()
+}
